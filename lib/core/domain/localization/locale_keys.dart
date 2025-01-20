7d2d6f114f83e575950543b2ecee9c9aea5df3d3
Mahmoud Abulmagd <me@abulmagd.dev>
2025-01-21 00:39:18 +0200

    Init Commit

---
+++ lib/core/domain/localization/locale_keys.dart
@@ -0,0 +1,10 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class LocaleKeys {
+  static const aboutMe = 'aboutMe';
+  static const services = 'services';
+  static const testimonials = 'testimonials';
+  static const projects = 'projects';
+  static const contactMe = 'contactMe';
+  static const name = 'name';
+}
